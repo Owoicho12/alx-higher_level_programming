cfd49c261589f3752561323dd9aeda4823f143db
Joshua Owoichoche Amah <115462208+Owoicho12@users.noreply.github.com>
2023-04-12 23:37:56 +0100

    Create 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines read_file function."""
+
+
+def read_file(filename=""):
+    """Perform a print to stdout of a file.
+
+    Args:
+        filename (str): String representation of file name to read
+
+    Returns:
+        None
+    """
+    with open(filename) as file:
+        text = file.read()
+    print(text, end="")
+    return None
