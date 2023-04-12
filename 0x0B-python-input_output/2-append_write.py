a3985d47700b30a756dafd1c0b56a0162de82b88
Joshua Owoichoche Amah <115462208+Owoicho12@users.noreply.github.com>
2023-04-12 23:41:35 +0100

    Create 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines append_write function."""
+
+
+def append_write(filename="", text=""):
+    """Appends to @filename contents of @text.
+
+    Args:
+        filename (str, optional): File name
+        text (str, optional): Content to append in @filename
+
+    Returns:
+        Number of characters appended
+    """
+    with open(filename, encoding="UTF-8", mode="a") as file:
+        char_count = file.write(text)
+    return char_count
